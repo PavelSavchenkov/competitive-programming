35f209c8b34063a8d42442ac7ba8efb0988c8f74
Pavel Savchenkov <savchenkov@adorable.ai>
2018-08-11 21:03:07 +0300

    minor changes

--- prewritten-code/templates/macros.hpp
+++ prewritten-code/templates/macros.hpp
@@ -4,18 +4,18 @@
 #define MAKE_FINAL_NAME(NAME, NUM) MAKE_FINAL_NAME_HELPER(NAME ## _, NUM)
 #define ARGUMENTS_SIZE_HELPER(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, COUNT, ...) COUNT
 #define ARGUMENTS_SIZE(...) ARGUMENTS_SIZE_HELPER(__VA_ARGS__, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
-#define SELECT_MACROS(NAME, ...) MAKE_FINAL_NAME(NAME, ARGUMENTS_SIZE(__VA_ARGS__))(__VA_ARGS__)
+#define SELECT_MACRO(NAME, ...) MAKE_FINAL_NAME(NAME, ARGUMENTS_SIZE(__VA_ARGS__))(__VA_ARGS__)
 
 #define pb push_back
 #define sz(c) int((c).size())
 #define all(c) begin(c), end(c)
 #define mp(x, y) make_pair(x, y)
 
-#define ford(...) SELECT_MACROS(ford, __VA_ARGS__)
+#define ford(...) SELECT_MACRO(ford, __VA_ARGS__)
 #define ford_3(i, s, n) for (int i = int(n) - 1; i >= int(s); --i)
 #define ford_2(i, n) ford_3(i, 0, n)
 
-#define forn(...) SELECT_MACROS(forn, __VA_ARGS__)
+#define forn(...) SELECT_MACRO(forn, __VA_ARGS__)
 #define forn_3(i, s, n) for (int i = int(s); i < int(n); ++i)
 #define forn_2(i, n) forn_3(i, 0, n)
 
